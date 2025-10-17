7ddbaa89326c8907d801888119556513b56fb77a
schneiderdauinheimeringrid-pixel <schneiderdauinheimeringrid@gmail.com>
2025-10-17 16:58:05 -0300

    Minha Lista de Tarefas - 3

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ const txt_nova_tarefa = document.querySelector("#txt-nova-tarefa");
 const btn_nova_tarefa = document.querySelector("#btn-nova-tarefa");
 // Procura pelo elemento com o ID "btn-nova-tarefa" no documento HTML
 const lista_tarefas = document.querySelector("#lista-tarefas");
+
+
 // Carrega o áudio reproduzido ao Concluir uma tarefa
 const audioConcluir = new Audio('sound/gmae.wav');
+// Força o navegador a carregar o áudio para evitar atrasos na reprodução
+audioConcluir.preload = "auto";
+
+// Variável global que controla a exibição da modal "Excluir tarefa"
+const modalExcluir = new bootstrap.Modal(document.getElementById('exampleModal'));
+
+// Variável global que armazena a tarefa que será excluída
+let id_tarefa_excluir;
 
 // ---------------------------------------------------------
 // 2. FUNÇÕES DE LÓGICA
@@ -28,28 +38,28 @@ function iniciaToDo() {
 function adicionarTarefa() {
     // Se a caixa de texto de "Adicionar nova tarefa" não está vazia
     // .trim() remove espaços em branco do começo e fim do calor do campo
-        if (txt_nova_tarefa.value.trim() !== "") {
-             const btn_item = `
+    if (txt_nova_tarefa.value.trim() !== "") {
+        const btn_item = `
                 <div>
-                    <button class="btn btn-success btn-sm me-2"onclick="concluirTarefa(this)">Concluir</button>
-                    <button class="btn btn-danger btn-sm">Excluir</button>
+                    <button class="btn btn-success btn-sm me-2 btn-concluir"onclick="concluirTarefa(this)">Concluir</button>
+                    <button class="btn btn-danger btn-sm btn-excluir" onclick="obterIDTarefaExcluir(this);modalExcluir.show();">Excluir</button>
                 </div>
     `;
-
-    // Cria um novo item de lista
-    const item = document.createElement("li");
-    item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
-    // Adiciona o texto digitado na caixa de texto e os botões para concluir e excluir a tarefa.
-    // "span" permite aplicar formação em linha
-    //"w-75" limita o nome da tarefa á 75% do tamanho da linha, deixando 25% do tamanho que excedem 75% do tamanho da linha
-    // "text-truncate" corta e adiciona reticencias (tres pontos...) em nomes de tarefas que excedem 75% do tamanh da linha
-    item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value + "</span>" + btn_item;
-
-    // Adicona o item a lista de tarefas
-    lista_tarefas.append(item);
-
+        
+        // Cria um novo item de lista
+        const item = document.createElement("li");
+        item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+        // Adiciona o texto digitado na caixa de texto e os botões para concluir e excluir a tarefa.
+        // "span" permite aplicar formação em linha
+        //"w-75" limita o nome da tarefa á 75% do tamanho da linha, deixando 25% do tamanho que excedem 75% do tamanho da linha
+        // "text-truncate" corta e adiciona reticencias (tres pontos...) em nomes de tarefas que excedem 75% do tamanh da linha
+        item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value + "</span>" + btn_item;
+        
+        // Adicona o item a lista de tarefas
+        lista_tarefas.append(item);
+        
     }
-     // Limpa o campo de texto de "Adicionar nova tarefa" após adicionar a tarefa a lista
+    // Limpa o campo de texto de "Adicionar nova tarefa" após adicionar a tarefa a lista
     txt_nova_tarefa.value = "";
     // Seleciona o campo "Adicionar nova tarefa" após adicionar a tarefa a lista
     txt_nova_tarefa.focus();
@@ -62,18 +72,40 @@ function adicionarTarefaEnter(evento) {
     }
 }
 
-function concluirTarefa(elemento) {
+function concluirTarefa(btn_concluir) {
     audioConcluir.play();
-
+    
     // Jogar 100 confettis na tela
     for (let i = 0; i <= 100; i++) {
         confetti();
     }
+
+    // Atualiza o ID da tarefa a ser excluída e passa como parametro o botão de "Concluir" clickado.
+    obterIDTarefaExcluir(btn_concluir);
+
+    // Chama a função JS "excluirTarefa()".
+    excluirTarefa();
+}  
+
+function excluirTarefa() {
+    // Remove o item da lista de tarefas
+    lista_tarefas.removeChild(lista_tarefas.children[id_tarefa_excluir]);
+    // Fecha modal de "Excluir tarefa"
+    modalExcluir.hide();
+}
+
+function obterIDTarefaExcluir (btn) {
+    // Encontra o elemento HTML "li" (item) pai mais próximo do
+    // botão de "Concluir" ou "Excluir" clickado.
+    // Perceba que a função JS "obterIDTarefaExcluir()", o botão clickado é
+    // recebido como parametro da função (btn).
+    const item = btn.closest("li");
+    const tarefas = Array.from(lista_tarefas.children);
+    id_tarefa_excluir = tarefas.indexOf(item);
     
 }
 // ---------------------------------------------------------
 // 3. ESCUTADORES DE EVENTOS E INÍCIO
 // ---------------------------------------------------------
-    
+
 iniciaToDo();
-    
